f4238df7655bddfc14eeabfe683569ef05f2158b
Artsiom <Loonaticc@gmail.com>
2020-06-06 13:10:46 +0300

    add: create structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ExchangeRatesComponent } from './exchange-rates/exchange-rates.component';
+import { BanksComponent } from './banks/banks.component';
+import { NewsComponent } from './news/news.component';
+import { ConverterComponent } from './converter/converter.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+ declarations: [
+  AppComponent,
+  MainPageComponent,
+  ExchangeRatesComponent,
+  BanksComponent,
+  NewsComponent,
+  ConverterComponent,
+ ],
+ imports: [BrowserModule, AppRoutingModule],
+ providers: [],
+ bootstrap: [AppComponent],
 })
 export class AppModule {}
