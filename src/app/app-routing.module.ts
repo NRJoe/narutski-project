901a58272cb07add0eec08d4810937c53cad7448
Artsiom <Loonaticc@gmail.com>
2020-06-07 20:54:38 +0300

    add: base HTML structure; add: base css styles.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BanksComponent } from './banks/banks.component';
+import { ConverterComponent } from './converter/converter.component';
+import { ExchangeRatesComponent } from './exchange-rates/exchange-rates.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ErrorComponent } from './error/error.component';
+import { NewsComponent } from './news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'about', component: AboutComponent },
+ { path: 'banks', component: BanksComponent },
+ { path: 'converter', component: ConverterComponent },
+ { path: 'rates', component: ExchangeRatesComponent },
+ { path: 'news', component: NewsComponent },
+ { path: '', component: MainPageComponent },
+ { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
  imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,29 @@ import { BanksComponent } from './banks/banks.component';
 import { NewsComponent } from './news/news.component';
 import { ConverterComponent } from './converter/converter.component';
 import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
- declarations: [
-  AppComponent,
-  MainPageComponent,
-  ExchangeRatesComponent,
-  BanksComponent,
-  NewsComponent,
-  ConverterComponent,
-  AboutComponent,
- ],
- imports: [BrowserModule, AppRoutingModule],
- providers: [],
- bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        MainPageComponent,
+        ExchangeRatesComponent,
+        BanksComponent,
+        NewsComponent,
+        ConverterComponent,
+        AboutComponent,
+        ErrorComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
